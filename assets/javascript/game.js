63eb77eff644fc9e5490a9c3d7676776a6241fc6
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-03-08 09:54:45 -0600

    Formatting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,18 @@
 $(document).ready(function () {
-    //select a random number to be the score 19-120
-    var targetScore = Math.floor(Math.random() * 120) + 19;
-
-
-    //select a number for each button to use 1-12
-    // var btn1 = Math.floor(Math.random() * 12) + 1;
-    // var btn2 = Math.floor(Math.random() * 12) + 1;
-    // var btn3 = Math.floor(Math.random() * 12) + 1;
-    // var btn4 = Math.floor(Math.random() * 12) + 1;
-
-    // $("#button-1").val(btn1);
-    // $("#button-2").val(btn2);
-    // $("#button-3").val(btn3);
-    // $("#button-4").val(btn4);
-    // $("#target-score").text(targetScore);
-
-    // console.log($("#button-1").val())
-
-
     //set the users score, wins, and losses
     var userScore = 0;
     var wins = 0;
     var losses = 0;
 
     function reset() {
+        //select a random number to be the score 19-120
         targetScore = Math.floor(Math.random() * 120) + 19;
+        //select a random number to be assigned to each button
         var btn1 = Math.floor(Math.random() * 12) + 1;
         var btn2 = Math.floor(Math.random() * 12) + 1;
         var btn3 = Math.floor(Math.random() * 12) + 1;
         var btn4 = Math.floor(Math.random() * 12) + 1;
+        //reset the user score to 0
         userScore = 0;
 
 
@@ -55,10 +39,6 @@ $(document).ready(function () {
     $(".btn").on("click", function () {
         userScore += parseInt($(this).val());
         $("#user-score").text(userScore);
-        // debugger;
-
-        //if score less than target score 
-        //keep going
 
         // if score and target are equal 
         if (userScore === targetScore) {
